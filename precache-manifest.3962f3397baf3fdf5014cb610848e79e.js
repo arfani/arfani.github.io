4a728418a93dcd1e5dd209cd3d068154fccec44f
arfani <arfanihidayat@gmail.com>
2019-01-31 13:46:49 +0800

    init

---
+++ precache-manifest.3962f3397baf3fdf5014cb610848e79e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "328f93d6e3e98b22de3e",
+    "url": "/static/js/main.328f93d6.chunk.js"
+  },
+  {
+    "revision": "5b218ddc45ae587943a9",
+    "url": "/static/js/1.5b218ddc.chunk.js"
+  },
+  {
+    "revision": "5b218ddc45ae587943a9",
+    "url": "/static/css/1.b7dce76c.chunk.css"
+  },
+  {
+    "revision": "dba79128b7cec40db59671af14c3e04d",
+    "url": "/index.html"
+  }
+];
