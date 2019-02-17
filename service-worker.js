f7c698d0259dd69e1816f3d6a243091375c6c08c
arfani hidayat <arfanihidayat@gmail.com>
2019-02-17 22:26:38 +0800

    fix bhs&sasak

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c712396160b1119f716f4235a476e2d.js"
+  "/precache-manifest.d8b6564a1a2496147c7eb5e749bc6a51.js"
 );
 
 workbox.clientsClaim();
