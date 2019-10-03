e38861b556aefabd571e9635ede17b6338a7952d
rfun <arfanihidayat@gmail.com>
2019-10-03 11:51:04 +0800

    add login form

---
+++ precache-manifest.d68270b6a5317fc746f57175e7b7e501.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b62affbbe24e8f3e30373d91bc92335",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1b22d27a0975469d4dc",
+    "url": "/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "374313c4ae5b4e4877df",
+    "url": "/static/css/main.0f30b6e8.chunk.css"
+  },
+  {
+    "revision": "b1b22d27a0975469d4dc",
+    "url": "/static/js/2.c7527e8c.chunk.js"
+  },
+  {
+    "revision": "374313c4ae5b4e4877df",
+    "url": "/static/js/main.06866647.chunk.js"
+  },
+  {
+    "revision": "cfc2f67d5cb0c2734fda",
+    "url": "/static/js/runtime-main.d21d1913.js"
+  },
+  {
+    "revision": "f5a3d35439cf4fec8b9e0df7e25e0f2e",
+    "url": "/static/media/CV.f5a3d354.png"
+  },
+  {
+    "revision": "14e2550831bf300dfb1f819449539dca",
+    "url": "/static/media/InvMansa.14e25508.png"
+  },
+  {
+    "revision": "13d1921764b910a59fcb695341ee6e3e",
+    "url": "/static/media/LoginHotspot.13d19217.gif"
+  },
+  {
+    "revision": "74ebe77a0994251e6711cc4005c930b3",
+    "url": "/static/media/RankingForki.74ebe77a.png"
+  },
+  {
+    "revision": "a5fb21a8666b4d1b7a6b6aba5ab4d033",
+    "url": "/static/media/ResHotel.a5fb21a8.png"
+  },
+  {
+    "revision": "676a9592b229acf73756d0eb089f06a0",
+    "url": "/static/media/bg-head.676a9592.png"
+  },
+  {
+    "revision": "3953d19abf8a0c27cf498954b57848db",
+    "url": "/static/media/bg.3953d19a.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f5a3d35439cf4fec8b9e0df7e25e0f2e",
+    "url": "/static/media/pp.f5a3d354.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d2b1fee592742687bdb7dfd7f525f37.js"
+  "/precache-manifest.d68270b6a5317fc746f57175e7b7e501.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
